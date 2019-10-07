7ccbca10714bd10921288a0733940abde300e716
Conor Shields <cshields236@gmail.com>
2019-10-07 23:29:52 +0100

    new class Profile

--- src/entities/Subscriber.java
+++ src/entities/Subscriber.java
@@ -4,24 +4,40 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToOne;
+
+//Create the Querys
+@NamedQueries({@NamedQuery(name = "Subscriber.findAll", query = "select o from Subscriber o"),
+		@NamedQuery(name = "Subscriber.findByUsername", query = "select o from Subscriber o where o.username=:username")
+})
 
 @Entity
 public class Subscriber {
 
-	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+
+
+	//Subscriber has one profile (Has a string descrription)
+
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
+
 	private String username;
 	private String password;
+
+	@OneToOne
+	private Profile subscriberProfile;
+
 
 	public Subscriber() {
 
 	}
 
-	public Subscriber(String username, String password) {
-
-		this.username = username;
-		this.password = password;
+	public Subscriber(String username, String password, Profile subprof) {
+		this.username=username;
+		this.password=password;
+		this.subscriberProfile=subprof;
 	}
 
 	public int getId() {
@@ -47,7 +63,6 @@ public class Subscriber {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
 
 
 }
